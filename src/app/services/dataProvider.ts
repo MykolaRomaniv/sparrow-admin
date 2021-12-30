95b9c8bc0152e50a6688a91cf657d29012cce702
Mykola Romaniv <mykolaromaniw@gmail.com>
2021-12-30 21:32:44 +0200

    fix: images handling

--- src/app/services/dataProvider.ts
+++ src/app/services/dataProvider.ts
@@ -1,7 +1,7 @@
+import { IMAGE_KEY } from "constants/index"
 import firebase from "firebase/app"
 import firebaseDataProvider from "ra-data-firebase-client"
 import firebaseConfig from "services/firebaseConfig"
-import { convertFileToBase64 } from "utils"
 
 const defaultFirebase = !firebase.apps.length
   ? firebase.initializeApp(firebaseConfig)
@@ -9,7 +9,7 @@ const defaultFirebase = !firebase.apps.length
 
 defaultFirebase.analytics()
 
-const settings = { context: "", imagekey: "images", filekey: "files" }
+const settings = { context: "", imagekey: IMAGE_KEY, filekey: "files" }
 
 const dataProvider = firebaseDataProvider(firebase, settings)
 
@@ -17,40 +17,6 @@ const database = defaultFirebase.database()
 
 const customDataProvider = {
   ...dataProvider,
-  update: (
-    resource: string,
-    params: { id: string; data: any; previousData: any },
-  ) => {
-    if (resource !== "products" || !params.data.pictures) {
-      // fallback to the default implementation
-      return dataProvider.update(resource, params)
-    }
-    /**
-     * For products update only, convert uploaded image in base 64 and attach it to
-     * the `picture` sent property, with `src` and `title` attributes.
-     */
-
-    // Freshly dropped pictures are File objects and must be converted to base64 strings
-    const newPictures = params.data.pictures
-
-    return Promise.all(newPictures.map(convertFileToBase64))
-      .then((base64Pictures) =>
-        base64Pictures.map((picture64, i) => ({
-          id: i,
-          src: picture64,
-          title: `${params.data.pictures[i].title}`,
-        })),
-      )
-      .then((transformedNewPictures) =>
-        dataProvider.update(resource, {
-          ...params,
-          data: {
-            ...params.data,
-            pictures: [...transformedNewPictures],
-          },
-        }),
-      )
-  },
   getMany: (source: string, params: { ids: string[] }) => {
     const resource = [settings.context, source].join("/")
     const getMany = new Promise((resolve) => {
